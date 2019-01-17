54ccacc6d7c83b5e441e6b9775eb6dbe3bebc767
yans2 <yans2@rose-hulman.edu>
2019-01-17 14:51:47 -0500

    done

--- src/m3_nested_loops_in_printing.py
+++ src/m3_nested_loops_in_printing.py
@@ -12,9 +12,9 @@ def main():
     # run_test_rectangle_of_stars()
     # run_test_triangle_of_stars()
     # run_test_decreasing_exclamation_marks()
-    run_test_alternating_brackets()
+    # run_test_alternating_brackets()
     # run_test_triangle_same_number_in_each_row()
-    # run_test_triangle_all_numbers_in_each_row()
+    run_test_triangle_all_numbers_in_each_row()
 
 
 def run_test_rectangle_of_stars():
@@ -250,7 +250,7 @@ def triangle_same_number_in_each_row(r):
     Precondition:  r is a non-negative integer.
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -258,7 +258,10 @@ def triangle_same_number_in_each_row(r):
     #   in this or the other problems in this module, as doing so
     #   would defeat the goal of providing practice at loops within loops.
     # ------------------------------------------------------------------
-
+    for k in range(1,r+1):
+        for j in range(1,k+1):
+            print(k,end="")
+        print()
 
 def run_test_triangle_all_numbers_in_each_row():
     """ Tests the    triangle_all_numbers_in_each_row    function. """
@@ -293,7 +296,7 @@ def triangle_all_numbers_in_each_row(r):
     Precondition:  r is a non-negative integer.
     """
     # ------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # DONE: 7. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -301,8 +304,10 @@ def triangle_all_numbers_in_each_row(r):
     #   in this or the other problems in this module, as doing so
     #   would defeat the goal of providing practice at loops within loops.
     # ------------------------------------------------------------------
-
-
+    for k in range(1,r+1):
+        for j in range(1,k+1):
+            print(j,end="")
+        print()
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # ----------------------------------------------------------------------
