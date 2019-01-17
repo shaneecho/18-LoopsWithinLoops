0f5cac4cd8f37a2f3de60efeb08982bc027541f7
yans2 <yans2@rose-hulman.edu>
2019-01-17 14:32:33 -0500

    done

--- src/m3_nested_loops_in_printing.py
+++ src/m3_nested_loops_in_printing.py
@@ -11,8 +11,8 @@ def main():
     """ Calls the other functions to test them. """
     # run_test_rectangle_of_stars()
     # run_test_triangle_of_stars()
-    run_test_decreasing_exclamation_marks()
-    # run_test_alternating_brackets()
+    # run_test_decreasing_exclamation_marks()
+    run_test_alternating_brackets()
     # run_test_triangle_same_number_in_each_row()
     # run_test_triangle_all_numbers_in_each_row()
 
@@ -201,7 +201,7 @@ def alternating_brackets(m, n):
     Precondition:  m and n are positive integers with m >= n.
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -209,7 +209,13 @@ def alternating_brackets(m, n):
     #   in this or the other problems in this module, as doing so
     #   would defeat the goal of providing practice at loops within loops.
     # ------------------------------------------------------------------
-
+    for k in range(m,n-1,-1):
+        for j in range(k):
+            if j % 2 == 0:
+                print('[', end="")
+            if j % 2 == 1:
+                print(']', end="")
+        print()
 
 def run_test_triangle_same_number_in_each_row():
     """ Tests the    triangle_same_number_in_each_row    function. """
