afb4e1e48a1841e7b99494db5155a2b5bd18655b
yans2 <yans2@rose-hulman.edu>
2019-01-17 16:30:39 -0500

    done

--- src/m4_nested_loops_in_sequences.py
+++ src/m4_nested_loops_in_sequences.py
@@ -117,7 +117,7 @@ def multiply_numbers(sequence_of_lists):
        [FYI: This 'can be multiplied ...' is an example of DUCK TYPING.]
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #  ** READ THE TESTS that have been written for you (ABOVE).
     #  ** ASK QUESTIONS if you do not understand the TESTS (ABOVE).
     #
@@ -128,8 +128,12 @@ def multiply_numbers(sequence_of_lists):
     #             to loop through it in the INNER loop.
     #        -- See   m2e_nested_loops_in_sequences   as needed.
     # ------------------------------------------------------------------
+    for j in range(len(sequence_of_lists)):
 
+        sublist = sequence_of_lists[j]
 
+        for k in range(len(sublist)):
+            sublist[k] = sublist[k] * (j + 1)
 
 def run_test_sum_numbers():
     """ Tests the    sum_numbers    function. """
