2f20a9419f1854f081546c10cbfe6ffcab1753e4
yans2 <yans2@rose-hulman.edu>
2019-01-21 16:34:02 -0500

    done

--- src/m6_nested_loops_in_graphics.py
+++ src/m6_nested_loops_in_graphics.py
@@ -147,7 +147,7 @@ def draw_wall_on_right(rectangle, n, window):
     and n is a small, positive integer.
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
     upperleftx = rectangle.get_upper_left_corner().x
@@ -158,16 +158,18 @@ def draw_wall_on_right(rectangle, n, window):
     y1 = upperlefty
     x2 = lowerrightx
     y2 = lowerrighty
+    width = rectangle.get_width()
+    height = rectangle.get_height()
     for k in range(n):
-        for j in range(n):
+        for j in range(k+1):
             new_rectangle = rg.Rectangle(rg.Point(x1,y1),rg.Point(x2,y2))
             new_rectangle.attach_to(window)
             window.render(0.1)
 
-            x1 = x1 - n*(x2-x1)
-            x2 = x2 - n*(x2-x1)
-        y1 = y1 + n*(x2-x1)
-        y2 = y2 + n*(x2-x1)
+            x1 = x1 - width
+            x2 = x2 - width
+        y1 = y1 + height
+        y2 = y2 + height
         x1 = upperleftx
         x2 = lowerrightx
 
